ed29c2bb822bf2b2b471aca003f22b367b2bf6ba
성승연 <alwayssingyo@gmail.com>
2025-12-30 17:55:10 +0900

    feat: 레니스 스크롤 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
   import Gnb from "$lib/components/gnb/Gnb.svelte";
   import "$lib/assets/styles/global.scss";
+  import Lenis from "@studio-freight/lenis";
+
+  let lenis: Lenis;
+  let rafId: number;
+
+  onMount(() => {
+    lenis = new Lenis({
+      lerp: 0.08,
+      wheelMultiplier: 1,
+      touchMultiplier: 1,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      rafId = requestAnimationFrame(raf);
+    };
+
+    rafId = requestAnimationFrame(raf);
+  });
 
   export let data;
 </script>
